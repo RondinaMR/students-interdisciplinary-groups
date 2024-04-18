e976df895c10b96f10793d95eddb334710b2b83f
mr <marco.rondina@studenti.polito.it>
2024-04-18 10:40:15 +0200

    Save a second xlsx extended file with student course column

--- main.py
+++ main.py
@@ -79,3 +79,43 @@ if __name__ == '__main__':
         sheet.column_dimensions[column_letter].width = adjusted_width
     # Save the modified Excel file
     workbook.save('students-output.xlsx')
+
+    #save to xslx - extended
+    students[['GRUPPO', 'MATRICOLA', 'COGNOME', 'NOME', course_column]].sort_values(['GRUPPO', 'COGNOME'], ascending=True).to_excel('students-output.xlsx', sheet_name="Gruppi P&T 2024", index=False)
+    
+    # Read the existing Excel file
+    workbook = openpyxl.load_workbook('students-output.xlsx')
+    sheet = workbook['Gruppi P&T 2024']
+    
+    # Define the fill colors
+    white_fill = PatternFill(start_color="FFFFFF", end_color="FFFFFF", fill_type="solid")
+    grey_fill = PatternFill(start_color="EFEFEF", end_color="EFEFEF", fill_type="solid")
+
+    # Apply the fill colors and borders based on the "GRUPPO" column
+    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=4):
+        group_value = row[0].value
+        if ord(group_value) % 2 == 0:
+            fill_color = white_fill
+        else:
+            fill_color = grey_fill
+        for cell in row:
+            cell.fill = fill_color
+            cell.border = openpyxl.styles.Border(left=openpyxl.styles.Side(style='thin'),
+                                                 right=openpyxl.styles.Side(style='thin'),
+                                                 top=openpyxl.styles.Side(style='thin'),
+                                                 bottom=openpyxl.styles.Side(style='thin'))
+    # Resize the column width to show all the content
+    for column in sheet.columns:
+        max_length = 0
+        column_letter = column[0].column_letter
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except:
+                pass
+        adjusted_width = (max_length + 2) * 1.2
+        sheet.column_dimensions[column_letter].width = adjusted_width
+    # Save the modified Excel file
+    workbook.save('students-output-extended.xlsx')
+
